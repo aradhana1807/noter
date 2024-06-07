8c8c370f54a5a3da868fb22bcfbbc6fcb24577c2
Aradhana Nayak <aradhana.zero@gmail.com>
2024-06-07 12:28:43 +0530

    added final authentication

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,23 +1,51 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { Spinnaker } from "next/font/google";
+import Link from "next/link";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Noter</span>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Noter</span>
       </h1>
 
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Noter is the connected workspace where <br/>
+        Noter is the connected workspace where <br />
         better, more organized, and efficient work happens.
       </h3>
 
-      <Button>
-        Enter Noter
-        <ArrowRight className="ml-2 h-4 w-4"/>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Noter
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Noter Free
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
-  )
-}
+  );
+};
